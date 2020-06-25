15389def86fac06ac8df521f39c8d5562576c29b
Sean Harvey <halkyon@gmail.com>
2020-06-25 00:19:25 +0000

    fix always changing route table for nat gateway

--- main.tf
+++ main.tf
@@ -89,8 +89,8 @@ resource "aws_route_table" "private" {
   count  = var.num_zones
   vpc_id = aws_vpc.vpc.id
   route {
-    cidr_block = "0.0.0.0/0"
-    gateway_id = aws_nat_gateway.nat[count.index].id
+    cidr_block     = "0.0.0.0/0"
+    nat_gateway_id = aws_nat_gateway.nat[count.index].id
   }
   tags = {
     Name = "${var.name}-private-${data.aws_availability_zones.available.names[count.index]}"
