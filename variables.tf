8d4f454af9c351bc02587430f25522b3426291d2
Sean Harvey <halkyon@gmail.com>
2020-07-02 01:20:00 +0000

    add types to variables

--- variables.tf
+++ variables.tf
@@ -1,17 +1,21 @@
 variable "name" {
+  type        = string
   description = "Name of the cluster."
 }
 
 variable "region" {
+  type        = string
   description = "AWS region the cluster will reside in."
 }
 
 variable "vpc_cidr" {
+  type        = string
   default     = "10.0.0.0/16"
   description = "VPC CIDR."
 }
 
 variable "subnet_addtl_bits" {
+  type        = number
   default     = 4
   description = <<EOF
 Additional bits added to VPC CIDR, to determine subnet size.
@@ -20,6 +24,7 @@ EOF
 }
 
 variable "num_zones" {
+  type        = number
   default     = 2
   description = <<EOF
 Number of availability zones the cluster will reside in.
@@ -29,16 +34,19 @@ EOF
 }
 
 variable "disk_size" {
+  type        = number
   default     = 100
   description = "Disk size for nodes, in GB."
 }
 
 variable "instance_type" {
+  type        = string
   default     = "t3.medium"
   description = "Instance type for nodes"
 }
 
 variable "desired_size" {
+  type        = number
   default     = 2
   description = <<EOF
 Autoscaling group desired size for nodes. This value is only used initially on cluster creation.
@@ -48,11 +56,13 @@ EOF
 }
 
 variable "min_size" {
+  type        = number
   default     = 2
   description = "Autoscaling group minimum size for nodes."
 }
 
 variable "max_size" {
+  type        = number
   default     = 4
   description = "Autoscaling group maximum size for nodes."
 }
